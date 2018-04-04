e2daf36f8f36fa3d27701d8efcb8b0a2cd0c4ad5
Espen Henriksen <dev@henriksen.is>
2018-04-04 17:57:43 +0200

    We already build in build step, drop in push script

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t "$DOCKER_USERNAME/$DOCKER_REPONAME" .
 docker push "$DOCKER_USERNAME/$DOCKER_REPONAME"
