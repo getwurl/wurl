b324e5977b088266b80c1442e87dfb351fe9a32c
Espen Henriksen <dev@henriksen.is>
2018-04-16 21:11:12 +0200

    WiP static build

--- ci/script.sh
+++ ci/script.sh
@@ -3,6 +3,16 @@
 set -ex
 
 main() {
+    if [ -z $STATIC ]; then
+      echo Enabling static build
+      export OPENSSL_STATIC=true
+      export OPENSSL_LIB_DIR=/usr/lib
+      export OPENSSL_INCLUDE_DIR=/usr/include/openssl
+      export LIBZ_SYS_STATIC=1
+      export PKG_CONFIG_ALLOW_CROSS=true
+      export PKG_CONFIG_ALL_STATIC=true
+    fi
+
     cargo fmt --all -- --write-mode=diff
     cross build --target $TARGET --release
 
